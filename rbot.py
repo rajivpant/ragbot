be1dd194340281c9c7a5f4644acf0df993636fe4
Rajiv Pant <rajiv@rajiv.com>
2023-05-07 13:43:09 -0400

    added command line args for decorator and prompt

--- rbot.py
+++ rbot.py
@@ -5,33 +5,44 @@
 # Current version by Rajiv Pant
 
 import os
-from sys import argv
+import sys
+import argparse
 import openai
 
-# set your custom conversation decorator here
-with open('../rajiv-llms/fine-tuning/hearst.md', 'r') as file:
-    # Read the contents of the file into a string variable
-    CONVERSATION_DECORATOR = file.read()
-
-# Load your API key from an environment variable or secret management service
+# Load the OpenAI API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def chat(
-        prompt,
-        history = [],
-        conversation_decorator = CONVERSATION_DECORATOR,
-        model = 'gpt-4',
-        max_tokens = 1000,
-        stream = True,
-        request_timeout = 15,
-        temperature = .75
-    ):
 
-    if history == [] and conversation_decorator != '':
+def chat(prompt,
+         history=None,
+         conversation_decorator=None,
+         model='gpt-4',
+         max_tokens=1000,
+         stream=True,
+         request_timeout=15,
+         temperature=0.75):
+    """
+    Send a request to the OpenAI API with the provided prompt and optional parameters.
+
+    :param prompt: The user's input to generate a response for.
+    :param history: A list of prior messages in the conversation, if any.
+    :param conversation_decorator: Additional context to provide for the model.
+    :param model: The name of the GPT model to use (default is 'gpt-4').
+    :param max_tokens: The maximum number of tokens to generate in the response (default is 1000).
+    :param stream: Whether to stream the response from the API (default is True).
+    :param request_timeout: The request timeout in seconds (default is 15).
+    :param temperature: The creativity of the response, with higher values being more creative (default is 0.75).
+    :return: The generated response text from the model.
+    """
+    if history is None:
+        history = []
+
+    if conversation_decorator and not history:
         history.append({
             'role': 'system',
             'content': conversation_decorator,
         })
+
     history = history + [{
         'role': 'user',
         'content': prompt
@@ -50,29 +61,30 @@ def chat(
 
     response = ''
 
-    for token in completion_method(
-        **args
-    ):
-        text = None
-        if 'content' in token['choices'][0]['delta']:
-            text = token['choices'][0]['delta']['content']
-            response = response + text
+    for token in completion_method(**args):
+        text = token['choices'][0]['delta'].get('content')
+        if text:
+            response += text
 
     return response
 
-#####
+
+def main():
+    parser = argparse.ArgumentParser(description="A GPT-4 based chatbot that generates responses based on user prompts.")
+    parser.add_argument('-p', '--prompt', required=True, help="The user's input to generate a response for.")
+    parser.add_argument('-d', '--decorator', help="Path to the conversation decorator file.")
+    args = parser.parse_args()
+
+    if args.decorator:
+        with open(args.decorator, 'r') as file:
+            conversation_decorator = file.read()
+    else:
+        conversation_decorator = None
+
+    reply = chat(prompt=args.prompt, conversation_decorator=conversation_decorator)
+
+    print(reply)
 
 
-prompt = None
-
-if argv[1:]:
-    prompt = argv[1]
-else:
-    print("Usage: python chat-gpt.py [prompt]")
-    exit()
-
-reply = chat(prompt=prompt)
-
-print(reply)
-
-
+if __name__ == "__main__":
+    main()
