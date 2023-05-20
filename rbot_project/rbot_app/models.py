3a7807ea1ee907cf7a876b68c0341f4ab9808ec4
Rajiv Pant <rajiv@rajiv.com>
2023-05-19 22:09:56 -0400

    removed incomplete web version

--- rbot_project/rbot_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Decorator(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
--- rbot_project/rbot_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'rbot_app'
-
-urlpatterns = [
-    path('', views.get_response, name='get_response'),
-]
