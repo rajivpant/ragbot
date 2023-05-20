3f3e2cde65fb80529962850657e9269c7be3d814
Rajiv Pant <rajiv@rajiv.com>
2023-05-19 22:09:56 -0400

    removed incomplete web version

--- rbot_project/rbot_app/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class RbotForm(forms.Form):
-    message = forms.CharField(label='Your message', max_length=1000, widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}))
-    decorator = forms.ChoiceField(choices=[('none', 'None'), ('bold', 'Bold'), ('italic', 'Italic'), ('underline', 'Underline')])
